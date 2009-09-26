dd093adc5091c365580c4b2df7db1767230a370d
Bernie Thuman <bernard.thuman@vangent.com>
2009-09-26 00:32:14 +0000

    Supplemented documentation.

--- src/xutil/src/com/vangent/hieos/xutil/db/support/SQLConnectionWrapper.java
+++ src/xutil/src/com/vangent/hieos/xutil/db/support/SQLConnectionWrapper.java
@@ -25,21 +25,36 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 /**
+ * Centralized class to get database connection instances given a JNDI/JDBC
+ * resource name.
  *
  * @author Bernie Thuman
  */
 public class SQLConnectionWrapper {
 
     // Place here to centralize naming (not a great solution, but it works).
+    /**
+     * HIEOS logger JNDI/JDBC resource name.
+     */
     static final public String logJNDIResourceName = "jdbc/hieos-log";
+    /**
+     * HIEOS ADT JNDI/JDBC resource name.
+     */
     static final public String adtJNDIResourceName = "jdbc/hieos-adt";
+    /**
+     * HIEOS Registry JNDI/JDBC resource name.
+     */
     static final public String registryJNDIResourceName = "jdbc/hieos-registry";
+    /**
+     * HIEOS Repository JNDI/JDBC resource name.
+     */
     static final public String repoJNDIResourceName = "jdbc/hieos-repo";
 
     /**
+     * Returns a Connection instance for a given JNDI resource name.
      *
-     * @param jndiResourceName
-     * @return
+     * @param jndiResourceName  Name of JNDI resource.
+     * @return Database connection.
      * @throws XdsInternalException
      */
     public Connection getConnection(String jndiResourceName) throws XdsInternalException {
