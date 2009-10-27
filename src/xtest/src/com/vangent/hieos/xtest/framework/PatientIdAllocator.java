a6c4c59072518ab4cfaab5885c232537aee9b9c1
Bernie Thuman <bernard.thuman@vangent.com>
2009-10-27 20:38:56 +0000

    Added ability to generate a new patient id without caching.

--- src/xtest/src/com/vangent/hieos/xtest/framework/PatientIdAllocator.java
+++ src/xtest/src/com/vangent/hieos/xtest/framework/PatientIdAllocator.java
@@ -10,7 +10,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package com.vangent.hieos.xtest.framework;
 
 import com.vangent.hieos.xutil.exception.ExceptionUtil;
@@ -23,67 +22,92 @@ import java.io.IOException;
 import java.io.PrintStream;
 
 public class PatientIdAllocator extends IdAllocator {
-	static String current_pid = null; 
 
-	public void loadCurrent() throws XdsInternalException {
-		try {
-			current_pid =  getPatientIdBase() + "^^^" + getAssigningAuthority();
-		} catch (Exception e) {
-			throw new XdsInternalException(ExceptionUtil.exception_details(e));
-		}
-	}
+    static String current_pid = null;
 
-	public PatientIdAllocator() throws XdsInternalException {
-		loadCurrent();
-	}
+    public void loadCurrent() throws XdsInternalException {
+        try {
+            current_pid = getPatientIdBase() + "^^^" + getAssigningAuthority();
+        } catch (Exception e) {
+            throw new XdsInternalException(ExceptionUtil.exception_details(e));
+        }
+    }
 
-	public PatientIdAllocator(String patient_id) {
-		current_pid = patient_id;
-	}
+    public PatientIdAllocator() throws XdsInternalException {
+        loadCurrent();
+    }
 
-	String getAssigningAuthority() throws IOException {
-		return Io.stringFromFile(new File(assigning_authority_file)).trim();
-	}
+    public PatientIdAllocator(String patient_id) {
+        current_pid = patient_id;
+    }
 
-	String getPatientIdBase() throws IOException {
-		return Io.stringFromFile(new File(patient_id_base_file)).trim();
-	}
+    String getAssigningAuthority() throws IOException {
+        return Io.stringFromFile(new File(assigning_authority_file)).trim();
+    }
 
-	String getPatientIdIncr() throws IOException {
-		return Io.stringFromFile(new File(patient_id_incr_file)).trim();
-	}
+    String getPatientIdBase() throws IOException {
+        return Io.stringFromFile(new File(patient_id_base_file)).trim();
+    }
 
-	void putPatientIdIncr(String incr) throws IOException {
-		PrintStream ps = new PrintStream(new File(patient_id_incr_file));
-		ps.print(incr);
-		ps.close();
-	}
+    String getPatientIdIncr() throws IOException {
+        return Io.stringFromFile(new File(patient_id_incr_file)).trim();
+    }
 
-	// by default - one PatientID per execution - NO MORE
-	// new spec - always return id in configuration
-	public String allocate() throws XdsInternalException {
-		try {
-			if (current_pid == null) {
-				loadCurrent();
-			}
-			return current_pid;
-		} catch (Exception e) {
-			throw new XdsInternalException(ExceptionUtil.exception_details(e));
-		}
-	}
+    void putPatientIdIncr(String incr) throws IOException {
+        PrintStream ps = new PrintStream(new File(patient_id_incr_file));
+        ps.print(incr);
+        ps.close();
+    }
 
-	public String allocate_new() throws XdsInternalException {
-		if (TestConfig.pid_allocate_endpoint != null && !TestConfig.pid_allocate_endpoint.equals("")) {
-			try {
-				current_pid = HttpClient.httpGet(TestConfig.pid_allocate_endpoint);
-				TestConfig.rememberPatientId(current_pid);
-			} catch (Exception e) {
-				throw new XdsInternalException(ExceptionUtil.exception_details(e));
-			}
-		}
-		else 
-			System.out.println("WARNING: PID allocation service not configured, using Patient ID coded in test or tool");
-		return current_pid;
-	}
+    // by default - one PatientID per execution - NO MORE
+    // new spec - always return id in configuration
+    public String allocate() throws XdsInternalException {
+        try {
+            if (current_pid == null) {
+                loadCurrent();
+            }
+            return current_pid;
+        } catch (Exception e) {
+            throw new XdsInternalException(ExceptionUtil.exception_details(e));
+        }
+    }
 
+    /**
+     *
+     * @return
+     * @throws XdsInternalException
+     */
+    public String allocate_new() throws XdsInternalException {
+        if (TestConfig.pid_allocate_endpoint != null && !TestConfig.pid_allocate_endpoint.equals("")) {
+            try {
+                current_pid = this.allocate_new_pid();
+                TestConfig.rememberPatientId(current_pid);
+            } catch (Exception e) {
+                throw new XdsInternalException(ExceptionUtil.exception_details(e));
+            }
+        } else {
+            System.out.println("WARNING: PID allocation service not configured, using Patient ID coded in test or tool");
+        }
+        return current_pid;
+    }
+    
+    /**
+     * 
+     * @return
+     * @throws XdsInternalException
+     */
+    public String allocate_new_pid() throws XdsInternalException {
+        String new_pid = null;
+        if (TestConfig.pid_allocate_endpoint != null && !TestConfig.pid_allocate_endpoint.equals("")) {
+            try {
+                new_pid = HttpClient.httpGet(TestConfig.pid_allocate_endpoint);
+                new_pid = new_pid.trim();
+            } catch (Exception e) {
+                throw new XdsInternalException(ExceptionUtil.exception_details(e));
+            }
+        } else {
+            System.out.println("WARNING: PID allocation service not configured, using Patient ID coded in test or tool");
+        }
+        return new_pid;
+    }
 }
