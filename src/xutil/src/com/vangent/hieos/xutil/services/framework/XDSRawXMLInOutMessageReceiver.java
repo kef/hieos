463c308046370e186c1c86b325f3a5dc4a0e287b
Bernie Thuman <bernard.thuman@vangent.com>
2009-11-13 05:04:30 +0000

    CP420 changes.

--- src/xutil/src/com/vangent/hieos/xutil/services/framework/XDSRawXMLInOutMessageReceiver.java
+++ src/xutil/src/com/vangent/hieos/xutil/services/framework/XDSRawXMLInOutMessageReceiver.java
@@ -10,43 +10,33 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package com.vangent.hieos.xutil.services.framework;
 
-
-import com.vangent.hieos.xutil.services.framework.AbstractXDSRawXMLINoutMessageReceiver;
 import com.vangent.hieos.xutil.soap.SoapActionFactory;
 
 import org.apache.axis2.context.MessageContext;
 import org.apache.axis2.engine.MessageReceiver;
 
-public class XDSRawXMLInOutMessageReceiver extends   AbstractXDSRawXMLINoutMessageReceiver
+/**
+ *
+ * @author thumbe
+ */
+public class XDSRawXMLInOutMessageReceiver extends AbstractXDSRawXMLINoutMessageReceiver
+        implements MessageReceiver {
 
-implements MessageReceiver {
-	
-//	private static final Map<String, String> actions =
-//		new HashMap<String, String>()
-//		{
-//		
-//		     {
-//		    	 put("urn:ihe:iti:2007:ProvideAndRegisterDocumentSet-b", "urn:ihe:iti:2007:ProvideAndRegisterDocumentSet-bResponse");
-//		    	 put("urn:ihe:iti:2007:RegisterDocumentSet-b",           "urn:ihe:iti:2007:RegisterDocumentSet-bResponse");
-//		    	 put("urn:ihe:iti:2007:RetrieveDocumentSet",             "urn:ihe:iti:2007:RetrieveDocumentSetResponse");
-//		    	 put("urn:ihe:iti:2007:RegistryStoredQuery",             "urn:ihe:iti:2007:RegistryStoredQueryResponse");
-//		    	 put("urn:ihe:iti:2007:CrossGatewayRetrieve",            "urn:ihe:iti:2007:CrossGatewayRetrieveResponse");
-//		    	 put("urn:ihe:iti:2007:CrossGatewayQuery",               "urn:ihe:iti:2007:CrossGatewayQueryResponse");
-//		     }
-//		
-//		};
+    /**
+     *
+     * @param msgContext
+     * @param newmsgContext
+     */
+    public void validate_action(MessageContext msgContext, MessageContext newmsgContext) {
+        String in_action = msgContext.getWSAAction();
 
-	public void validate_action(MessageContext msgContext, MessageContext newmsgContext) {
-		String in_action = msgContext.getWSAAction();
-		
-		String out_action = SoapActionFactory.getResponseAction(in_action);
-		if (out_action == null) {
-			newmsgContext.setFailureReason(new Exception("Unknown action <" + in_action + ">"));
-			return;
-		}
-		newmsgContext.setWSAAction(out_action);
-	}
+        String out_action = SoapActionFactory.getResponseAction(in_action);
+        if (out_action == null) {
+            newmsgContext.setFailureReason(new Exception("Unknown action <" + in_action + ">"));
+            return;
+        }
+        newmsgContext.setWSAAction(out_action);
+    }
 }
