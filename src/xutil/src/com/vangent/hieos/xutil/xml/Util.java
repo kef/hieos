92fcc85ac066611079a4594d7e14d604f6483f9d
Bernie Thuman <bernard.thuman@vangent.com>
2010-07-27 01:32:50 +0000

    General cleanup and refactoring.  Also removed "toString()" usage in deep_copy() method.

--- src/xutil/src/com/vangent/hieos/xutil/xml/Util.java
+++ src/xutil/src/com/vangent/hieos/xutil/xml/Util.java
@@ -10,7 +10,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package com.vangent.hieos.xutil.xml;
 
 import com.vangent.hieos.xutil.exception.XdsInternalException;
@@ -19,7 +18,7 @@ import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.InputStream;
+//import java.io.InputStream;
 
 import javax.xml.parsers.FactoryConfigurationError;
 import javax.xml.stream.XMLInputFactory;
@@ -29,98 +28,93 @@ import javax.xml.stream.XMLStreamReader;
 import org.apache.axiom.om.OMElement;
 import org.apache.axiom.om.impl.builder.StAXOMBuilder;
 
+/**
+ *
+ * @author NIST (adapted by Bernie Thuman)
+ */
 public class Util {
-	
-	
-	public static OMElement parse_xml(Object o) throws FactoryConfigurationError, XdsInternalException {
-		if (o instanceof String) return parse_xml((String) o);
-		if (o instanceof InputStream) return parse_xml((InputStream) o);
-		if (o instanceof File) return parse_xml((File) o);
-		throw new XdsInternalException("Util.parse_xml(): do not understand input format " + o.getClass().getName());
-	}
 
-	public static OMElement parse_xml(InputStream is) throws FactoryConfigurationError, XdsInternalException {
+    /**
+     *
+     * @param infile
+     * @return
+     * @throws FactoryConfigurationError
+     * @throws XdsInternalException
+     */
+    public static OMElement parse_xml(File infile) throws FactoryConfigurationError, XdsInternalException {
+        // Create the parser
+        XMLStreamReader parser;
+        try {
+            parser = XMLInputFactory.newInstance().createXMLStreamReader(new FileInputStream(infile));
+        } catch (XMLStreamException e) {
+            throw new XdsInternalException("Could not create XMLStreamReader from " + infile.getName());
+        } catch (FileNotFoundException e) {
+            throw new XdsInternalException("Could not find input file " + infile.getAbsolutePath());
+        }
+        return Util.getRootElement(parser);
+    }
 
-//		create the parser
-		XMLStreamReader parser=null;
+    /**
+     *
+     * @param input
+     * @return
+     * @throws FactoryConfigurationError
+     * @throws XdsInternalException
+     */
+    public static OMElement parse_xml(String input) throws FactoryConfigurationError, XdsInternalException {
+        // Create the parser
+        XMLStreamReader parser;
+        try {
+            parser = XMLInputFactory.newInstance().createXMLStreamReader(new ByteArrayInputStream(input.getBytes()));
+        } catch (Exception e) {
+            throw new XdsInternalException("Could not create XMLStreamReader from string: " + input.substring(0, 100) + "...");
+        }
+        return Util.getRootElement(parser);
+    }
 
-		try {
-			parser = XMLInputFactory.newInstance().createXMLStreamReader(is);
-		} catch (XMLStreamException e) {
-			throw new XdsInternalException("Could not create XMLStreamReader from InputStream");
-		} 
+    /**
+     *
+     * @param parser
+     * @return
+     * @throws FactoryConfigurationError
+     * @throws XdsInternalException
+     */
+    private static OMElement getRootElement(XMLStreamReader parser) throws FactoryConfigurationError, XdsInternalException {
+        // create the builder
+        StAXOMBuilder builder = null;
+        try {
+            builder = new StAXOMBuilder(parser);
+        } catch (Exception e) {
+            throw new XdsInternalException("Could not create StAXOMBuilder: " + e.getMessage());
+        }
 
-//		create the builder
-		StAXOMBuilder builder = new StAXOMBuilder(parser);
+        OMElement documentElement = null;
+        try {
+            // get the root element (in this case the envelope)
+            documentElement = builder.getDocumentElement();
+            if (documentElement == null) {
+                throw new XdsInternalException("No document element");
+            }
+        } catch (Exception e) {
+            throw new XdsInternalException(
+                    "Could not create XMLStreamReader in Util.getRootElement():" + e.getMessage());
+        }
+        return documentElement;
+    }
 
-//		get the root element (in this case the envelope)
-		OMElement documentElement =  builder.getDocumentElement();	
-		if (documentElement == null)
-			throw new XdsInternalException("No document element");
-		return documentElement;
-	}
-
-	public static OMElement parse_xml(File infile) throws FactoryConfigurationError, XdsInternalException {
-
-//		create the parser
-		XMLStreamReader parser=null;
-
-		try {
-			parser = XMLInputFactory.newInstance().createXMLStreamReader(new FileInputStream(infile));
-		} catch (XMLStreamException e) {
-			throw new XdsInternalException("Could not create XMLStreamReader from " + infile.getName());
-		} catch (FileNotFoundException e) {
-			throw new XdsInternalException("Could not find input file " + infile.getAbsolutePath());
-		}
-
-//		create the builder
-		StAXOMBuilder builder = new StAXOMBuilder(parser);
-
-//		get the root element (in this case the envelope)
-		OMElement documentElement =  builder.getDocumentElement();	
-		if (documentElement == null)
-			throw new XdsInternalException("No document element");
-		return documentElement;
-	}
-
-	public static OMElement parse_xml(String input) throws FactoryConfigurationError, XdsInternalException {
-
-//		create the parser
-		XMLStreamReader parser=null;
-
-		try {
-			parser = XMLInputFactory.newInstance().createXMLStreamReader(new ByteArrayInputStream(input.getBytes()));
-		} catch (Exception e) {
-			throw new XdsInternalException("Could not create XMLStreamReader from string: " + input.substring(0, 100) + "...");
-		} 
-
-//		create the builder
-		StAXOMBuilder builder = null;
-		try {
-			 builder = new StAXOMBuilder(parser);
-		} catch (Exception e) {
-			throw new XdsInternalException("Could not create StAXOMBuilder: " + e.getMessage());
-		} 
-
-		OMElement documentElement = null; 
-		try {
-//			get the root element (in this case the envelope)
-			documentElement =  builder.getDocumentElement();	
-			if (documentElement == null)
-				throw new XdsInternalException("No document element");
-		} catch (Exception e) {
-			throw new XdsInternalException("Could not create XMLStreamReader (in Util.parse_xml()) from string: " + 
-					input.substring(0, (input.length() < 100) ? input.length() : 100) + 
-					"...");
-		}
-		return documentElement;
-	}
-
-	public static OMElement deep_copy(OMElement in) throws XdsInternalException {
-		if (in == null)
-			return null;
-		return parse_xml(in.toString());
-	}
-
-
+    /**
+     *
+     * @param in
+     * @return
+     * @throws XdsInternalException
+     */
+    public static OMElement deep_copy(OMElement in) throws XdsInternalException {
+        if (in == null) {
+            return null;
+        }
+        OMElement clonedElement = in.cloneOMElement();
+        clonedElement.build();
+        return clonedElement;
+        //return parse_xml(in.toString());
+    }
 }
