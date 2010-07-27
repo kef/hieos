ce2ec5643e215034135d65dcafae343db253b217
Bernie Thuman <bernard.thuman@vangent.com>
2010-07-27 01:35:51 +0000

    Added use of "XMLSchemaValidationEnabled" xconfig property usage.

--- src/xutil/src/com/vangent/hieos/xutil/registry/RegistryUtility.java
+++ src/xutil/src/com/vangent/hieos/xutil/registry/RegistryUtility.java
@@ -10,7 +10,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package com.vangent.hieos.xutil.registry;
 
 import com.vangent.hieos.xutil.response.RegistryErrorList;
@@ -21,58 +20,78 @@ import com.vangent.hieos.xutil.exception.XdsInternalException;
 import com.vangent.hieos.xutil.metadata.validation.Validator;
 import com.vangent.hieos.xutil.xml.SchemaValidation;
 import com.vangent.hieos.xutil.xlog.client.XLogMessage;
-
-import java.io.ByteArrayOutputStream;
-import java.io.PrintStream;
+import com.vangent.hieos.xutil.xconfig.XConfig;
 
 import org.apache.axiom.om.OMElement;
+import org.apache.log4j.Logger;
 
-
+/**
+ *
+ * @author NIST (Adapted by Bernie Thuman)
+ */
 public class RegistryUtility {
 
-	static public void schema_validate_local(OMElement ahqr, int metadata_type)
-	throws XdsInternalException, SchemaValidationException {
-		String schema_messages = null;
-		try {
-			schema_messages = SchemaValidation.validate_local(ahqr, metadata_type);
-		} catch (Exception e) {
-			throw new XdsInternalException("Schema Validation threw internal error: " + e.getMessage());
-		}
-		if (schema_messages != null && schema_messages.length() > 0)
-			throw new SchemaValidationException("Input did not validate against schema:" + schema_messages);
-	}
+    private final static Logger logger = Logger.getLogger(RegistryUtility.class);
 
-	static public RegistryErrorList metadata_validator(Metadata m, boolean is_submit) throws XdsException {
-		RegistryErrorList rel = new RegistryErrorList();
-		Validator v = new Validator(m, rel, is_submit, (XLogMessage)null);
-		v.run();
-		return rel;
-	}
-
-   
-	public static String exception_details(Exception e) {
-        /*
-		ByteArrayOutputStream baos = new ByteArrayOutputStream();
-		PrintStream ps = new PrintStream(baos);
-		e.printStackTrace(ps); */
-        e.printStackTrace();
-		return "Exception thrown: " + e.getClass().getName() + " : " + e.getMessage();
-		//return "Exception thrown: " + e.getClass().getName() + "\n" + e.getMessage() + "\n" + new String(baos.toByteArray());
+    /**
+     *
+     * @param ahqr
+     * @param metadata_type
+     * @throws XdsInternalException
+     * @throws SchemaValidationException
+     */
+    static public void schema_validate_local(OMElement ahqr, int metadata_type)
+            throws XdsInternalException, SchemaValidationException {
+        // Only do schema validation if required per configuration.
+        XConfig xconfig = XConfig.getInstance();
+        boolean XMLSchemaValidationEnabled = xconfig.getHomeCommunityPropertyAsBoolean(
+                "XMLSchemaValidationEnabled", false /* default (off) */);
+        if (XMLSchemaValidationEnabled == false) {
+            if (logger.isDebugEnabled()) {
+                logger.debug("**** HIEOS - XML Schema Validation DISABLED ****");
+            }
+            return;  // Early return!
+        }
+        if (logger.isDebugEnabled()) {
+            logger.debug("**** HIEOS - XML Schema Validation ENABLED ****");
+        }
+        String schema_messages = null;
+        try {
+            schema_messages = SchemaValidation.validate_local(ahqr, metadata_type);
+        } catch (Exception e) {
+            throw new XdsInternalException("Schema Validation threw internal error: " + e.getMessage());
+        }
+        if (schema_messages != null && schema_messages.length() > 0) {
+            throw new SchemaValidationException("Input did not validate against schema:" + schema_messages);
+        }
     }
 
-    
-	public static String exception_trace(Exception e) {
-		if (e == null) 
-			return "";
-		ByteArrayOutputStream baos = new ByteArrayOutputStream();
-		PrintStream ps = new PrintStream(baos);
-		StackTraceElement ste[] = e.getStackTrace();
-		ps.print("\n");
-		for (int i=0; i<ste.length && i<15; i++)
-			ps.print("\t" + ste[i].toString() + "\n");
-		//e.printStackTrace(ps);
+    /**
+     *
+     * @param m
+     * @param is_submit
+     * @return
+     * @throws XdsException
+     */
+    static public RegistryErrorList metadata_validator(Metadata m, boolean is_submit) throws XdsException {
+        RegistryErrorList rel = new RegistryErrorList();
+        Validator v = new Validator(m, rel, is_submit, (XLogMessage) null);
+        v.run();
+        return rel;
+    }
 
-		return new String(baos.toByteArray());
-	}
-
+    /**
+     *
+     * @param e
+     * @return
+     */
+    public static String exception_details(Exception e) {
+        /*
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        PrintStream ps = new PrintStream(baos);
+        e.printStackTrace(ps); */
+        e.printStackTrace();
+        return "Exception thrown: " + e.getClass().getName() + " : " + e.getMessage();
+        //return "Exception thrown: " + e.getClass().getName() + "\n" + e.getMessage() + "\n" + new String(baos.toByteArray());
+    }
 }
