3a80b4e742566a972ede8fdee12cd3609884f54a
Bernie Thuman <bernard.thuman@vangent.com>
2010-09-02 02:48:16 +0000

    Removed demographic data from ADT schema.

--- data/adt/mssqlserver/createadtddl.sql
+++ data/adt/mssqlserver/createadtddl.sql
@@ -36,10 +36,6 @@ CREATE TABLE patient (
   uuid VARCHAR(64) NOT NULL,
   id VARCHAR(100) NOT NULL,
   timestamp VARCHAR(40),
-  birthdatetime VARCHAR(40),
-  adminsex VARCHAR(20),
-  accountnumber VARCHAR(100),
-  bedid VARCHAR(100),
   status CHAR(1) NOT NULL DEFAULT 'A',
   CONSTRAINT patient_pkey PRIMARY KEY (id)
 );
@@ -49,57 +45,13 @@ CREATE TABLE patient (
 --
 CREATE INDEX patient_uuid_idx ON patient (uuid);
 
-
---
--- Definition of table patientaddress
---
-
-CREATE TABLE patientaddress (
-  parent VARCHAR(64) NOT NULL,
-  streetaddress VARCHAR(100) NOT NULL,
-  otherdesignation VARCHAR(64) NOT NULL,
-  city VARCHAR(32) NOT NULL,
-  stateorprovince VARCHAR(32) NOT NULL,
-  zipcode VARCHAR(12) NOT NULL,
-  country VARCHAR(32) NOT NULL,
-  countyorparish VARCHAR(32) NOT NULL,
-  CONSTRAINT patientaddress_pkey PRIMARY KEY (parent)
-);
-
-
---
--- Definition of table `patientname`
---
-
-CREATE TABLE patientname (
-  parent VARCHAR(64) NOT NULL,
-  familyname VARCHAR(32) NOT NULL,
-  givenname VARCHAR(32) NOT NULL,
-  secondandfurthername VARCHAR(32) NOT NULL,
-  suffix VARCHAR(12) NOT NULL,
-  prefix VARCHAR(12) NOT NULL,
-  degree VARCHAR(12) NOT NULL,
-  CONSTRAINT patientname_pkey PRIMARY KEY (parent)
-);
-
---
--- Definition of table `patientrace`
---
-
-CREATE TABLE patientrace (
-  parent VARCHAR(64) NOT NULL,
-  race VARCHAR(64) NOT NULL,
-  CONSTRAINT patientrace_pkey PRIMARY KEY (parent)
-);
-
-
 --
 -- Definition of table mergehistory
 --
 CREATE TABLE mergehistory (
   uniqueid VARCHAR(64) NOT NULL,
-  survivingpatientid VARCHAR(64) NOT NULL,
-  subsumedpatientid VARCHAR(64) NOT NULL,
+  survivingpatientid VARCHAR(100) NOT NULL,
+  subsumedpatientid VARCHAR(100) NOT NULL,
   action CHAR(1) NOT NULL,
   datetimeperformed DATETIME NOT NULL,
   CONSTRAINT mh_pkey PRIMARY KEY (uniqueid)
