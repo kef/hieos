c5a2595a03bf1c1945a7115a05486781eec98f6a
Bernie Thuman <bernard.thuman@vangent.com>
2011-04-20 17:09:38 +0000

    Added templates as configuration.

--- src/DocViewer/src/com/vangent/hieos/DocViewer/server/services/rpc/config/ConfigRemoteServiceImpl.java
+++ src/DocViewer/src/com/vangent/hieos/DocViewer/server/services/rpc/config/ConfigRemoteServiceImpl.java
@@ -12,12 +12,18 @@
  * limitations under the License.
  */package com.vangent.hieos.DocViewer.server.services.rpc.config;
 
+import java.util.List;
+
 import com.vangent.hieos.DocViewer.client.model.config.Config;
+import com.vangent.hieos.DocViewer.client.model.config.DocumentTemplateConfig;
 import com.vangent.hieos.DocViewer.client.services.rpc.ConfigRemoteService;
 import com.vangent.hieos.DocViewer.server.framework.ServletUtilMixin;
+import com.vangent.hieos.xutil.xconfig.XConfig;
+import com.vangent.hieos.xutil.xconfig.XConfigObject;
 import com.google.gwt.user.server.rpc.RemoteServiceServlet;
 
-public class ConfigRemoteServiceImpl extends RemoteServiceServlet implements ConfigRemoteService {
+public class ConfigRemoteServiceImpl extends RemoteServiceServlet implements
+		ConfigRemoteService {
 
 	/**
 	 * 
@@ -27,66 +33,98 @@ public class ConfigRemoteServiceImpl extends RemoteServiceServlet implements Con
 	@Override
 	public Config getConfig() {
 		System.out.println("********* ConfigRemoteServiceImpl ********");
-		
+
 		// Get the mixin to allow access to xconfig.xml.
 		ServletUtilMixin servletUtil = new ServletUtilMixin();
 		servletUtil.init(this.getServletContext());
 
 		// Create the Config instance that will be sent back to the client.
 		Config config = new Config();
-		
+
 		// Now get the relevant properties
-		
+
 		// DefaultSearchMode:
-		String defaultSearchMode = servletUtil.getProperty(Config.KEY_SEARCH_MODE);
+		String defaultSearchMode = servletUtil
+				.getProperty(Config.KEY_SEARCH_MODE);
 		System.out.println("DefaultSearchMode = " + defaultSearchMode);
-		if (defaultSearchMode == null)
-		{
+		if (defaultSearchMode == null) {
 			defaultSearchMode = Config.VAL_SEARCH_MODE_HIE;
 		}
-		
+
 		// TrimDocumentTabTitles:
-		String trimDocumentTabTitles = servletUtil.getProperty(Config.KEY_TRIM_DOCUMENT_TAB_TITLES);
-		if (trimDocumentTabTitles == null)
-		{
+		String trimDocumentTabTitles = servletUtil
+				.getProperty(Config.KEY_TRIM_DOCUMENT_TAB_TITLES);
+		if (trimDocumentTabTitles == null) {
 			trimDocumentTabTitles = "false";
 		}
-		
+
 		// TrimDocumentTabTitlesLength:
-		String trimDocumentTabTitlesLength = servletUtil.getProperty(Config.KEY_TRIM_DOCUMENT_TAB_TITLES_LENGTH);
-		if (trimDocumentTabTitlesLength == null)
-		{
+		String trimDocumentTabTitlesLength = servletUtil
+				.getProperty(Config.KEY_TRIM_DOCUMENT_TAB_TITLES_LENGTH);
+		if (trimDocumentTabTitlesLength == null) {
 			trimDocumentTabTitlesLength = "50";
 		}
 
 		// Title:
-		String title = servletUtil.getProperty(Config.KEY_TITLE);		
-		if (title == null)
-		{
+		String title = servletUtil.getProperty(Config.KEY_TITLE);
+		if (title == null) {
 			title = "HIEOS Doc Viewer";
 		}
-		
+
 		// LogoFileName:
-		String logoFileName = servletUtil.getProperty(Config.KEY_LOGO_FILE_NAME);
+		String logoFileName = servletUtil
+				.getProperty(Config.KEY_LOGO_FILE_NAME);
 		System.out.println("LogoFileName = " + logoFileName);
-		if (logoFileName == null)
-		{
+		if (logoFileName == null) {
 			logoFileName = "search_computer.png";
 		}
-		
+
 		// LogoWidth/LogoHeight:
 		String logoWidth = servletUtil.getProperty(Config.KEY_LOGO_WIDTH);
 		String logoHeigth = servletUtil.getProperty(Config.KEY_LOGO_HEIGHT);
-		
+
 		// Fill up the config:
 		config.put(Config.KEY_SEARCH_MODE, defaultSearchMode);
 		config.put(Config.KEY_TRIM_DOCUMENT_TAB_TITLES, trimDocumentTabTitles);
-		config.put(Config.KEY_TRIM_DOCUMENT_TAB_TITLES_LENGTH, trimDocumentTabTitlesLength);
+		config.put(Config.KEY_TRIM_DOCUMENT_TAB_TITLES_LENGTH,
+				trimDocumentTabTitlesLength);
 		config.put(Config.KEY_TITLE, title);
 		config.put(Config.KEY_LOGO_FILE_NAME, logoFileName);
 		config.put(Config.KEY_LOGO_WIDTH, logoWidth);
 		config.put(Config.KEY_LOGO_HEIGHT, logoHeigth);
-		
+
+		this.loadDocumentTemplateConfigs(servletUtil, config);
 		return config;
+	}
+
+	/**
+	 * 
+	 * @param servletUtil
+	 * @param config
+	 */
+	private void loadDocumentTemplateConfigs(ServletUtilMixin servletUtil,
+			Config config) {
+		try {
+			XConfig xconf = XConfig.getInstance();
+			XConfigObject propertiesObject = xconf.getXConfigObjectByName(
+					"DocViewerProperties", "DocViewerPropertiesType");
+			XConfigObject documentTemplateListConfig = propertiesObject
+					.getXConfigObjectWithName("DocumentTemplates",
+							"DocumentTemplateListType");
+			List<XConfigObject> configObjects = documentTemplateListConfig
+					.getXConfigObjectsWithType("DocumentTemplateType");
+			for (XConfigObject configObject : configObjects) {
+				DocumentTemplateConfig documentTemplateConfig = new DocumentTemplateConfig();
+				documentTemplateConfig.setDisplayName(configObject
+						.getProperty("DisplayName"));
+				documentTemplateConfig.setFileName(configObject
+						.getProperty("FileName"));
+				config.addDocumentTemplateConfig(documentTemplateConfig);
+			}
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
 	}
 }
