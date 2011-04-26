ae3e772c7628516ee66c7160970f743d3786b142
Bernie Thuman <bernard.thuman@vangent.com>
2011-04-26 18:52:36 +0000

    New class added.

---
+++ src/authutil/src/com/vangent/hieos/authutil/model/Role.java
@@ -0,0 +1,46 @@
+/*
+ * This code is subject to the HIEOS License, Version 1.0
+ *
+ * Copyright(c) 2011 Vangent, Inc.  All rights reserved.
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.vangent.hieos.authutil.model;
+
+/**
+ *
+ * @author Bernie Thuman
+ */
+public class Role {
+
+    private String name;
+
+    /**
+     *
+     * @param name
+     */
+    public Role(String name) {
+        this.name = name;
+    }
+
+    /**
+     *
+     * @return
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * 
+     * @param name
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+}
