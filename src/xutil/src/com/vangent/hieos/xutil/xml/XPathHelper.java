3f28ef74b013047f52677b0e10d27becabe5466f
Bernie Thuman <bernard.thuman@vangent.com>
2011-06-07 00:34:02 +0000

    Added more XPathHelper utility methods.

--- src/xutil/src/com/vangent/hieos/xutil/xml/XPathHelper.java
+++ src/xutil/src/com/vangent/hieos/xutil/xml/XPathHelper.java
@@ -1,7 +1,7 @@
 /*
  * This code is subject to the HIEOS License, Version 1.0
  *
- * Copyright(c) 2010 Vangent, Inc.  All rights reserved.
+ * Copyright(c) 2010-2011 Vangent, Inc.  All rights reserved.
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -20,7 +20,7 @@ import org.apache.log4j.Logger;
 import org.jaxen.JaxenException;
 
 /**
- * A utility to simply usage of XPath.
+ * A utility to simplify XPath usage.
  *
  * @author Bernie Thuman
  */
@@ -33,16 +33,36 @@ public class XPathHelper {
      *
      * @param rootNode The starting node (OMElement) for the search.
      * @param xpathExpression The XPATH expression.
-     * @param nameSpace  Value of name space to use (e.g. "urn:hl7-org:v3"). May be null.
+     * @param namespaceURI  Value of namespace to use (e.g. "urn:hl7-org:v3"). May be null.
      * @return OMElement null if not found.
+     * @throws XPathHelperException
      */
     static public OMElement selectSingleNode(
-            OMElement rootNode, String xpathExpression, String nameSpace) throws XPathHelperException {
+            OMElement rootNode, String xpathExpression, String namespaceURI) throws XPathHelperException {
+        String[] namespacePrefixes = new String[1];
+        String[] namespaceURIs = new String[1];
+        namespacePrefixes[0] = "ns";
+        namespaceURIs[0] = namespaceURI;
+        return XPathHelper.selectSingleNode(rootNode, xpathExpression, namespacePrefixes, namespaceURIs);
+    }
+
+    /**
+     * Return a single OMElement given the XPATH expression.
+     *
+     * @param rootNode The starting node (OMElement) for the search.
+     * @param xpathExpression The XPATH expression.
+     * @param namespacePrefixes List of namespace prefixes.
+     * @param namespaceURIs List of namespace URIs (parallel array to namespacePrefixes).
+     * @return OMElement null if not found.
+     * @throws XPathHelperException
+     */
+    static public OMElement selectSingleNode(
+            OMElement rootNode, String xpathExpression, String[] namespacePrefixes, String[] namespaceURIs) throws XPathHelperException {
         OMElement resultNode = null;
         try {
             AXIOMXPath xpath = new AXIOMXPath(xpathExpression);
-            if (nameSpace != null) {
-                xpath.addNamespace("ns", nameSpace /* "urn:hl7-org:v3" */);
+           for (int i = 0; i < namespacePrefixes.length; i++) {
+                xpath.addNamespace(namespacePrefixes[i], namespaceURIs[i]);
             }
             resultNode = (OMElement) xpath.selectSingleNode(rootNode);
             if (resultNode != null) {
@@ -56,21 +76,41 @@ public class XPathHelper {
         return resultNode;
     }
 
+
     /**
      * Return a List of OMElements given the XPATH expression.
      *
      * @param rootNode The starting node (OMElement) for the search.
      * @param xpathExpression The XPATH expression.
-     * @param nameSpace  Value of name space to use (e.g. "urn:hl7-org:v3").  May be null.
+     * @param namespaceURI  Value of namespace URI to use (e.g. "urn:hl7-org:v3").  May be null.
      * @return List<OMElement> null if not found.
      */
     static public List<OMElement> selectNodes(
-            OMElement rootNode, String xpathExpression, String nameSpace) throws XPathHelperException {
+            OMElement rootNode, String xpathExpression, String namespaceURI) throws XPathHelperException {
+        String[] namespacePrefixes = new String[1];
+        String[] namespaceURIs = new String[1];
+        namespacePrefixes[0] = "ns";
+        namespaceURIs[0] = namespaceURI;
+        return XPathHelper.selectNodes(rootNode, xpathExpression, namespacePrefixes, namespaceURIs);
+    }
+
+    /**
+     * Return a List of OMElements given the XPATH expression.
+     *
+     * @param rootNode The starting node (OMElement) for the search.
+     * @param xpathExpression The XPATH expression.
+     * @param namespacePrefixes List of namespace prefixes.
+     * @param namespaceURIs List of namespace URIs (parallel array to namespacePrefixes).
+     * @return List<OMElement> null if not found.
+     * @throws XPathHelperException
+     */
+    static public List<OMElement> selectNodes(
+            OMElement rootNode, String xpathExpression, String[] namespacePrefixes, String[] namespaceURIs) throws XPathHelperException {
         List resultNodes = null;
         try {
             AXIOMXPath xpath = new AXIOMXPath(xpathExpression);
-            if (nameSpace != null) {
-                xpath.addNamespace("ns", nameSpace /* "urn:hl7-org:v3" */);
+            for (int i = 0; i < namespacePrefixes.length; i++) {
+                xpath.addNamespace(namespacePrefixes[i], namespaceURIs[i]);
             }
             resultNodes = xpath.selectNodes(rootNode);
             if (resultNodes != null) {
