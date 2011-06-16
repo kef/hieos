c9863fc539c8a2970684f25859c5c8e2264ca8f9
Bernie Thuman <bernard.thuman@vangent.com>
2011-06-16 18:01:12 +0000

    Added license info.

--- src/pdp/src/com/vangent/hieos/services/pdp/transactions/PDPRequestHandler.java
+++ src/pdp/src/com/vangent/hieos/services/pdp/transactions/PDPRequestHandler.java
@@ -1,6 +1,14 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ * This code is subject to the HIEOS License, Version 1.0
+ *
+ * Copyright(c) 2011 Vangent, Inc.  All rights reserved.
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package com.vangent.hieos.services.pdp.transactions;
 
@@ -51,7 +59,7 @@ import org.picketlink.identity.federation.saml.v2.profiles.xacml.protocol.XACMLA
 
 /**
  *
- * @author thumbe
+ * @author Bernie Thuman
  */
 public class PDPRequestHandler extends XBaseTransaction {
 
