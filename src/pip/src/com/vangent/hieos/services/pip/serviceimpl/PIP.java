3740edbede9b45b7285eadaca02ca347ca5eedba
Bernie Thuman <bernard.thuman@vangent.com>
2011-07-08 19:49:11 +0000

    Added XDSBRIDGE

--- src/pip/src/com/vangent/hieos/services/pip/serviceimpl/PIP.java
+++ src/pip/src/com/vangent/hieos/services/pip/serviceimpl/PIP.java
@@ -12,12 +12,14 @@
  */
 package com.vangent.hieos.services.pip.serviceimpl;
 
+import com.vangent.hieos.services.pip.transactions.PIPRequestHandler;
 import com.vangent.hieos.xutil.exception.XMLParserException;
 import com.vangent.hieos.xutil.services.framework.XAbstractService;
 import com.vangent.hieos.xutil.xml.XMLParser;
 import org.apache.axiom.om.OMElement;
 import org.apache.axis2.AxisFault;
 import org.apache.axis2.context.ConfigurationContext;
+import org.apache.axis2.context.MessageContext;
 import org.apache.axis2.description.AxisService;
 import org.apache.log4j.Logger;
 
@@ -36,51 +38,22 @@ public class PIP extends XAbstractService {
      * @throws AxisFault
      */
     public OMElement GetConsentDirectives(OMElement request) throws AxisFault {
-        // FIXME (STUB):
-        String content =
-        "<pip:GetConsentDirectivesResponse xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns:pip=\"urn:hieos:policy:pip\">" +
-	"  <pip:ConsentDirectives alwaysAuthorize=\"false\">" +
-        "     <pip:AllowedOrganizations>" +
-        "        <pip:Organization>1.1</pip:Organization>" +
-        "        <pip:Organization>1.2</pip:Organization>" +
-        "     </pip:AllowedOrganizations>" +
-        "     <pip:BlockedOrganizations>" +
-        "        <pip:Organization>XXXX</pip:Organization>" +
-        "        <pip:Organization>XXXX</pip:Organization>" +
-        "     </pip:BlockedOrganizations>" +
-        "     <pip:BlockedIndividuals>" +
-	"        <pip:Individual>String</pip:Individual>" +
-	"        <pip:Individual>String</pip:Individual>" +
-        "     </pip:BlockedIndividuals>" +
-	"     <pip:AllowedRoles>" +
-	"        <pip:Role codeSystem=\"String\" displayName=\"String\" codeSystemName=\"String\" code=\"String\"/>" +
-	"        <pip:Role codeSystem=\"String\" displayName=\"String\" codeSystemName=\"String\" code=\"String\"/>" +
-	"     </pip:AllowedRoles>" +
-	"     <pip:AllowedPurposeOfUse>" +
-	"        <pip:PurposeOfUse codeSystem=\"String\" displayName=\"String\" codeSystemName=\"String\" code=\"String\"/>" +
-	"        <pip:PurposeOfUse codeSystem=\"String\" displayName=\"String\" codeSystemName=\"String\" code=\"String\"/>" +
-	"     </pip:AllowedPurposeOfUse>" +
-        "     <pip:SensitiveDocumentTypes>" +
-	"        <pip:DocumentType codeSystem=\"String\" displayName=\"String\" codeSystemName=\"String\" code=\"String\"/>" +
-	"        <pip:DocumentType codeSystem=\"String\" displayName=\"String\" codeSystemName=\"String\" code=\"String\"/>" +
-	"     </pip:SensitiveDocumentTypes>" +
-	"     <pip:SensitiveDocumentAccessList>" +
-	"        <pip:SensitiveDocumentAccess>" +
-	"           <pip:Organization>String</pip:Organization>" +
-	"           <pip:Individual>String</pip:Individual>" +
-	"        </pip:SensitiveDocumentAccess>" +
-	"        <pip:SensitiveDocumentAccess>" +
-	"           <pip:Organization>String</pip:Organization>" +
-	"           <pip:Individual>String</pip:Individual>" +
-	"        </pip:SensitiveDocumentAccess>" +
-        "     </pip:SensitiveDocumentAccessList>" +
-	"  </pip:ConsentDirectives>" +
-        "</pip:GetConsentDirectivesResponse>";
         try {
-            OMElement pipResponse = XMLParser.stringToOM(content);
-            return pipResponse;
-        } catch (XMLParserException ex) {
-            throw new AxisFault(ex.getMessage());
+            OMElement startup_error = beginTransaction(
+                    "PIP:GetConsentDirectives", request, XAbstractService.ActorType.PIP);
+            if (startup_error != null) {
+                // TBD: FIXUP (XUA should be returning a SOAP fault!)
+                return startup_error;
+            }
+            validateWS();
+            validateNoMTOM();
+            PIPRequestHandler handler =
+                    new PIPRequestHandler(this.log_message, MessageContext.getCurrentMessageContext());
+            OMElement result = handler.run(request);
+            endTransaction(handler.getStatus());
+            return result;
+        } catch (Exception ex) {
+            throw getAxisFault(ex);
         }
     }
 
