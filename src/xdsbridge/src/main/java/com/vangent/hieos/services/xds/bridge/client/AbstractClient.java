19d693fdf438bf1932eabdd7af74a295dcfa2eff
Bernie Thuman <bernard.thuman@vangent.com>
2011-07-28 13:10:30 +0000

    Moved Client class to xutil as WebServiceClient class.

--- src/xdsbridge/src/main/java/com/vangent/hieos/services/xds/bridge/client/AbstractClient.java
+++ src/xdsbridge/src/main/java/com/vangent/hieos/services/xds/bridge/client/AbstractClient.java
@@ -13,9 +13,9 @@
 
 package com.vangent.hieos.services.xds.bridge.client;
 
-import com.vangent.hieos.hl7v3util.client.Client;
 import com.vangent.hieos.hl7v3util.model.subject.DeviceInfo;
 import com.vangent.hieos.services.xds.bridge.support.XDSBridgeConfig;
+import com.vangent.hieos.xutil.soap.WebServiceClient;
 import com.vangent.hieos.xutil.xconfig.XConfigActor;
 
 /**
@@ -25,7 +25,7 @@ import com.vangent.hieos.xutil.xconfig.XConfigActor;
  * @version        v1.0, 2011-06-29
  * @author         Jim Horner
  */
-public abstract class AbstractClient extends Client {
+public abstract class AbstractClient extends WebServiceClient {
 
     /** Field description */
     private final XDSBridgeConfig xdsBridgeConfig;
