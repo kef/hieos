7f518d476341ed9fbec38b6a3218a4d9d2e38882
Bernie Thuman <bernard.thuman@vangent.com>
2011-08-12 16:04:05 +0000

    Incorporated use of HL7 v2 CNE format for policy evaluation purposes.

--- src/hl7v3util/src/com/vangent/hieos/hl7v3util/model/subject/CodedValue.java
+++ src/hl7v3util/src/com/vangent/hieos/hl7v3util/model/subject/CodedValue.java
@@ -24,43 +24,97 @@ public class CodedValue {
     private String codeSystemVersion;
     private String displayName;
 
+    /**
+     *
+     * @return
+     */
     public String getCode() {
         return code;
     }
 
+    /**
+     *
+     * @param code
+     */
     public void setCode(String code) {
         this.code = code;
     }
 
+    /**
+     *
+     * @return
+     */
     public String getCodeSystem() {
         return codeSystem;
     }
 
+    /**
+     *
+     * @param codeSystem
+     */
     public void setCodeSystem(String codeSystem) {
         this.codeSystem = codeSystem;
     }
 
+    /**
+     *
+     * @return
+     */
     public String getCodeSystemName() {
         return codeSystemName;
     }
 
+    /**
+     *
+     * @param codeSystemName
+     */
     public void setCodeSystemName(String codeSystemName) {
         this.codeSystemName = codeSystemName;
     }
 
+    /**
+     *
+     * @return
+     */
     public String getCodeSystemVersion() {
         return codeSystemVersion;
     }
 
+    /**
+     *
+     * @param codeSystemVersion
+     */
     public void setCodeSystemVersion(String codeSystemVersion) {
         this.codeSystemVersion = codeSystemVersion;
     }
 
+    /**
+     *
+     * @return
+     */
     public String getDisplayName() {
         return displayName;
     }
 
+    /**
+     *
+     * @param displayName
+     */
     public void setDisplayName(String displayName) {
         this.displayName = displayName;
+    }
+
+    /**
+     *
+     * @return
+     */
+    public String getCNEFormatted() {
+        if (code == null) {
+            return "UNKNOWN_CODE";
+        }
+        if (codeSystem == null || codeSystem.isEmpty()) {
+            return code;
+        }
+        return code + "^" + codeSystem;
     }
 }
