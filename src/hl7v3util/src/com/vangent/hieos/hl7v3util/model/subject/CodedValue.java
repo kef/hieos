fca652c9142354746af986b01e667a38d1af1bc3
Bernie Thuman <bernard.thuman@vangent.com>
2011-08-12 16:51:23 +0000

    Fixed bug in CNE data type usage.

--- src/hl7v3util/src/com/vangent/hieos/hl7v3util/model/subject/CodedValue.java
+++ src/hl7v3util/src/com/vangent/hieos/hl7v3util/model/subject/CodedValue.java
@@ -115,6 +115,6 @@ public class CodedValue {
         if (codeSystem == null || codeSystem.isEmpty()) {
             return code;
         }
-        return code + "^" + codeSystem;
+        return code + "^^" + codeSystem;
     }
 }
