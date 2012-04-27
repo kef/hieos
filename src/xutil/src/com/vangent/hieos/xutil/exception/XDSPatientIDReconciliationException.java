dafaec75446a19a50134cc34f1229a16399f3fb1
Bernie Thuman <bernard.thuman@vangent.com>
2012-04-27 11:54:14 +0000

    New exceptions for Metadata Update profile implementation.

--- src/xutil/src/com/vangent/hieos/xutil/exception/XDSPatientIDReconciliationException.java
+++ src/xutil/src/com/vangent/hieos/xutil/exception/XDSPatientIDReconciliationException.java
@@ -1,7 +1,7 @@
 /*
  * This code is subject to the HIEOS License, Version 1.0
  *
- * Copyright(c) 2008-2009 Vangent, Inc.  All rights reserved.
+ * Copyright(c) 2012 Vangent, Inc.  All rights reserved.
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -16,13 +16,13 @@ package com.vangent.hieos.xutil.exception;
  *
  * @author Bernie Thuman
  */
-public class XDSDocumentUniqueIdError extends XdsException {
+public class XDSPatientIDReconciliationException extends XdsException {
 
     /**
      *
      * @param msg
      */
-    public XDSDocumentUniqueIdError(String msg) {
+    public XDSPatientIDReconciliationException(String msg) {
         super(msg);
     }
 
@@ -31,7 +31,7 @@ public class XDSDocumentUniqueIdError extends XdsException {
      * @param msg
      * @param cause
      */
-    public XDSDocumentUniqueIdError(String msg, Throwable cause) {
+    public XDSPatientIDReconciliationException(String msg, Throwable cause) {
         super(msg, cause);
     }
 }
