9837d3bd34571667d790e53fdc32e7cd8a6fb2d2
Bernie Thuman <bernard.thuman@vangent.com>
2012-05-03 19:21:47 +0000

    Exception handling cleanup.

--- src/xcpd/src/com/vangent/hieos/services/xcpd/gateway/serviceimpl/XCPDInitiatingGatewayAsync.java
+++ src/xcpd/src/com/vangent/hieos/services/xcpd/gateway/serviceimpl/XCPDInitiatingGatewayAsync.java
@@ -13,7 +13,6 @@
 package com.vangent.hieos.services.xcpd.gateway.serviceimpl;
 
 import com.vangent.hieos.xutil.exception.SOAPFaultException;
-import com.vangent.hieos.xutil.exception.XdsWSException;
 
 import org.apache.log4j.Logger;
 
@@ -30,7 +29,7 @@ public class XCPDInitiatingGatewayAsync extends XCPDInitiatingGateway {
      * This method ensures that an asynchronous request has been sent. It evaluates the message
      * context to dtermine if "ReplyTo" is non-null and is not anonymous. It also ensures that
      * "MessageID" is non-null. It throws an exception if that is not the case.
-     * @throws XdsWSException
+     * @throws SOAPFaultException
      */
     @Override
     protected void validateWS() throws SOAPFaultException {
