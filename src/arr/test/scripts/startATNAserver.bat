538cc21fffcf05c162fc7e69d755169e32800811
Bernie Thuman <bernard.thuman@vangent.com>
2012-12-19 18:16:50 +0000

    ATNA test script update.

--- src/arr/test/scripts/startATNAserver.bat
+++ src/arr/test/scripts/startATNAserver.bat
@@ -6,14 +6,14 @@ for %%I IN (..\..\..\..\lib\mysql-connector-java-5.1.10-bin.jar) DO SET CP=!CP!;
 for %%I IN (..\..\..\..\lib\javaee.jar) DO SET CP=!CP!;%%I
 for %%I IN (..\..\..\..\lib\axis2-1.5\log4j-1.2.15.jar) DO SET CP=!CP!;%%I
 for %%I IN (..\..\..\..\lib\axis2-1.5\*.jar) DO SET CP=!CP!;%%I
-for %%I IN (c:\sun\sdk\lib\appserv-rt.jar) DO SET CP=!CP!;%%I
-for %%I IN (c:\sun\sdk\lib\appserv-admin.jar) DO SET CP=!CP!;%%I
-for %%I IN (c:\sun\sdk\lib\install\applications\jmsra\imqjmsra.jar) DO SET CP=!CP!;%%I
+for %%I IN (C:\glassfish\lib\appserv-rt.jar) DO SET CP=!CP!;%%I
+for %%I IN (C:\glassfish\lib\appserv-admin.jar) DO SET CP=!CP!;%%I
+for %%I IN (C:\glassfish\lib\install\applications\jmsra\imqjmsra.jar) DO SET CP=!CP!;%%I
 for %%I IN (..\..\..\xutil\dist\*.jar) DO SET CP=!CP!;%%I
 for %%I IN (..\..\dist\*.jar) DO SET CP=!CP!;%%I
  
-set keystore=C:/sun/sdk/domains/domain1/config/keystore.jks
-set truststore=C:/sun/sdk/domains/domain1/config/cacerts.jks
+set keystore=C:/glassfish/domains/hieos/config/keystore.jks
+set truststore=C:/glassfish/domains/hieos/config/cacerts.jks
 set keyrefs=-Djavax.net.ssl.keyStore=%keystore% -Djavax.net.ssl.keyStorePassword=changeit -Djavax.net.ssl.trustStore=%truststore% -Djavax.net.ssl.trustStorePassword=changeit
 set ciphers=-Dhttps.cipherSuites=SSL_RSA_WITH_3DES_EDE_CBC_SHA,TLS_RSA_WITH_AES_128_CBC_SHA
 
